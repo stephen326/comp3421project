db0e75888e847f1fe2b17ef4b8d1df7d96a3b082
stephen326 <xwq88866@gmail.com>
2025-04-19 19:00:06 +0800

    修改env

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,13 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // Railway 提供的数据库主机
-    user: process.env.DB_USER, // 数据库用户名
-    password: process.env.DB_PASSWORD, // 数据库密码
-    database: process.env.DB_NAME, // 数据库名称
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     waitForConnections: true,
-    connectionLimit: 10, // 最大连接数
-    queueLimit: 0
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 module.exports = pool;
