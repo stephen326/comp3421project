67b00f284cc83301783d206b5811c25842931427
stephen326 <xwq88866@gmail.com>
2025-04-19 18:56:38 +0800

    接着试

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
-
-pool.getConnection((err, conn) => {
-  if (err) {
-    console.error('❌ Failed to get connection from pool:', err);
-    return;
-  }
-  console.log('✅ Got connection from pool!');
-  conn.release(); // 用完记得放回连接池
+const pool = mysql.createPool({
+    host: process.env.DB_HOST, // Railway 提供的数据库主机
+    user: process.env.DB_USER, // 数据库用户名
+    password: process.env.DB_PASSWORD, // 数据库密码
+    database: process.env.DB_NAME, // 数据库名称
+    waitForConnections: true,
+    connectionLimit: 10, // 最大连接数
+    queueLimit: 0
 });
 
 module.exports = pool;
